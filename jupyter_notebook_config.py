44a767459d1ebb5c36b151d59e9b2c0a051c9afa
yusekiya <y.seki.14870@gmail.com>
2017-02-14 01:48:12 +0900

    Add pre save hook to remove output cells
    For version control system

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -11,3 +11,21 @@ c.MultiKernelManager.default_kernel_name = 'python3'
 if 'NOTEBOOK_PASSWORD' in os.environ:
   c.NotebookApp.password = passwd(os.environ['NOTEBOOK_PASSWORD'])
   del os.environ['NOTEBOOK_PASSWORD']
+
+# Trim output for version control system
+def scrub_output_pre_save(model, **kwargs):
+    """scrub output before saving notebooks"""
+    # only run on notebooks
+    if model['type'] != 'notebook':
+        return
+    # only run on nbformat v4
+    if model['content']['nbformat'] != 4:
+        return
+
+    for cell in model['content']['cells']:
+        if cell['cell_type'] != 'code':
+            continue
+        cell['outputs'] = []
+        cell['execution_count'] = None
+
+c.FileContentsManager.pre_save_hook = scrub_output_pre_save
